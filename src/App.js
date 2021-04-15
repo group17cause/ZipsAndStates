40f30cc35d90fbbb17970fb190cf4f4d05d2ec35
Richard Kozak <kozakrichard@users.noreply.github.com>
2021-04-15 17:38:38 -0400

    cleaning up

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import React, {Component} from 'react';
 import SearchZips from './components/SearchZips.js'
 //import SearchApi from './components/SearchApi.js'
 import SearchCities from './components/SearchCities.js'
-//import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-//import RouterHome from './components/RouterHome.js'
+
 import './App.css';
 
 function App () {
